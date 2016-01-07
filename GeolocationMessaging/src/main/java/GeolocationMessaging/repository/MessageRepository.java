01d128a95bf07dc973fad1b20bcbaf6249c442f4
jackjefferies <jack-jefferies@live.co.uk>
2016-01-07 21:38:06 +0000

    latest changes

---
+++ GeolocationMessaging/src/main/java/GeolocationMessaging/repository/MessageRepository.java
@@ -0,0 +1,11 @@
+package GeolocationMessaging.repository;
+
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+/**
+ * Created by Jack on 07/01/2016.
+ */
+public interface MessageRepository extends ElasticsearchRepository{
+
+
+}
